0febd56dc7b414720410ae790b4a138ffac580b2
chaews0327 <chaews0327@gmail.com>
2025-08-07 01:10:34 +0900

    Add: Model evaluation running script

---
+++ ecg_encoder/eval_scripts.sh
@@ -0,0 +1,7 @@
+python3 -m ecg_encoder.training.main \
+    --model coca_roberta-ViT-B-32 \
+    --config "./ecg_encoder/model/config.json" \
+    --ptbxl-path="./data" \
+    --resume "./ecg_encoder/checkpoints/epoch_10.pt" \
+    --lr 1e-4 \
+    --wd 0.1
