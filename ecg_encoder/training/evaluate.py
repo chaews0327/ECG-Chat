195b07efdd2ce1f884e349687fb3b79eaab6d502
chaews0327 <chaews0327@gmail.com>
2025-08-21 13:25:17 +0900

    Update: Add evaluation mode for test code

--- ecg_encoder/training/evaluate.py
+++ ecg_encoder/training/evaluate.py
@@ -9,12 +9,15 @@ import torch.nn.functional as F
 import random
 
 
-def test(model, data, epoch):
+def test(args, model, data, epoch):
     metrics = {}
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model.eval()
 
-    dataloader = data['test'].dataloader
+    if args.train:
+        dataloader = data['val'].dataloader
+    elif args.eval:
+        dataloader = data['test'].dataloader
     num_samples = 0
 
     all_ecg_features, all_text_features = [], []
