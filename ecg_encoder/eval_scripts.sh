7044cc7a0849f6bb7d214d648b93a5964ee830d5
chaews0327 <chaews0327@gmail.com>
2025-08-21 12:03:18 +0900

    Chore: Add WDE for eval

--- ecg_encoder/eval_scripts.sh
+++ ecg_encoder/eval_scripts.sh
@@ -1,8 +1,9 @@
 python3 -m ecg_encoder.training.main \
     --model coca_roberta-ViT-B-32 \
     --config "./ecg_encoder/model/config.json" \
-    --ptbxl-path="./data" \
-    --resume "./ecg_encoder/checkpoints/epoch_10.pt" \
+    --mimic-iv-ecg-path="/data/mimic-iv-ecg/physionet.org/files/mimic-iv-ecg/1.0" \
+    --resume "/home/chaewon/medicalai/my-ecg-chat/logs/mimic_wde/model_coca_roberta-ViT-B-32-lr_0.0001-b_64-p_amp/checkpoints/epoch_20.pt" \
     --lr 1e-4 \
     --wd 0.1 \
+    --wfep \
     --eval
